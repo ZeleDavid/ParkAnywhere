d2944b2998e71554bfa9d79ba009eefc7e3e4574
David Žele <david.zele@student.um.si>
2019-05-22 14:05:14 +0200

    dodan nov Angular app z templatom

---
+++ ParkWeb/sb-admin-material/src/app/layout/layout-routing.module.ts
@@ -0,0 +1,52 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LayoutComponent } from './layout.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            {
+                path: '',
+                redirectTo: 'dashboard'
+            },
+            {
+                path: 'dashboard',
+                loadChildren: './dashboard/dashboard.module#DashboardModule'
+            },
+            {
+                path: 'charts',
+                loadChildren: './charts/charts.module#ChartsModule'
+            },
+            {
+                path: 'components',
+                loadChildren:
+                    './material-components/material-components.module#MaterialComponentsModule'
+            },
+            {
+                path: 'forms',
+                loadChildren: './forms/forms.module#FormsModule'
+            },
+            {
+                path: 'grid',
+                loadChildren: './grid/grid.module#GridModule'
+            },
+            {
+                path: 'tables',
+                loadChildren: './tables/tables.module#TablesModule'
+            },
+            {
+                path: 'blank-page',
+                loadChildren: './blank-page/blank-page.module#BlankPageModule'
+            }
+        ]
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class LayoutRoutingModule {}
