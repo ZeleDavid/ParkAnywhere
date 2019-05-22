8b541bec844bdf12faa8e915da3cb510edaac702
lukacetina <luka.cetina@student.um.si>
2019-05-22 13:25:56 +0200

    dodal google maps v aplikacijo

--- ParkMobile/app/src/main/java/com/example/parkmobile/MainMapFragment.kt
+++ ParkMobile/app/src/main/java/com/example/parkmobile/MainMapFragment.kt
@@ -8,29 +8,32 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_test.*
 import android.content.Intent
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
 
 
-class MainMapFragment : Fragment(), OnMapReadyCallback {
 
-    private lateinit var mapView: MapView
-    private lateinit var map: GoogleMap
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        map = googleMap
-        val sydney = LatLng(-34.0, 151.0)
-        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
 
-    }
+
+class MainMapFragment : Fragment() {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(activity!!)
+
     }
 
     override fun onCreateView(
@@ -39,9 +42,39 @@ class MainMapFragment : Fragment(), OnMapReadyCallback {
     ): View? {
         val v = inflater.inflate(R.layout.fragment_test, container, false)
         // Inflate the layout for this fragment
-        val mapView = v.findViewById<MapView>(R.id.mapView)
-        mapView.onCreate(savedInstanceState)
-        mapView.getMapAsync(this)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
+        mapFragment.getMapAsync { mMap ->
+            mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+
+            mMap.clear() //clear old markers
+
+            val googlePlex = CameraPosition.builder()
+                .target(LatLng(46.554649, 15.645881))
+                .zoom(15f)
+                .bearing(0f)
+                .build()
+
+            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 1000, null)
+
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(LatLng(37.4219999, -122.0862462))
+                    .title("Spider Man")
+            )
+
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(LatLng(37.4629101, -122.2449094))
+                    .title("Iron Man")
+                    .snippet("His Talent : Plenty of money")
+            )
+
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(LatLng(37.3092293, -122.1136845))
+                    .title("Captain America")
+            )
+        }
         return v
     }
 
