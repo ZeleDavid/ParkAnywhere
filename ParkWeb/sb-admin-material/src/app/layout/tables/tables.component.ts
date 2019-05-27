59b3cda1165ad564cf14c2521ad00a42f1e970b8
David Žele <david.zele@student.um.si>
2019-05-27 17:58:46 +0200

    Prikazovanje parkirisc v tabelo

--- ParkWeb/sb-admin-material/src/app/layout/tables/tables.component.ts
+++ ParkWeb/sb-admin-material/src/app/layout/tables/tables.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 import {Observable} from 'rxjs';
-import { map, filter, switchMap } from 'rxjs/operators';
+import {map, filter, switchMap, tap} from 'rxjs/operators';
 import {HttpClient} from '@angular/common/http';
 import * as _ from 'lodash';
 
@@ -23,31 +23,36 @@ interface ParkirnaHisa {
     styleUrls: ['./tables.component.scss']
 })
 export class TablesComponent implements OnInit {
-    displayedColumns = ['id', 'name', 'zasedenost', 'color'];
-    dataSource: MatTableDataSource<UserData>;
+    displayedColumns = ['idParkirnaHisa', 'naziv', 'naslov', 'cenaNaUro', 'stZasedenihMest', 'stVsehMest', 'zasedenost', 'lastnik'];
+    dataSource: MatTableDataSource<ParkirnaHisa>;
     parkirneHise$: Observable<ParkirnaHisa[]>;
 
     @ViewChild(MatPaginator) paginator: MatPaginator;
     @ViewChild(MatSort) sort: MatSort;
 
     constructor(private http: HttpClient ) {
-        // Create 100 users
-        const users: UserData[] = [];
-        for (let i = 1; i <= 100; i++) {
-            users.push(createNewUser(i));
-        }
 
-        // Assign the data to the data source for the table to render
-        this.dataSource = new MatTableDataSource(users);
+        this.parkirneHise$ = this.http
+          .get<ParkirnaHisa[]>('http://localhost:8080/Docker_rest/REST/parkirneHise')
+          .pipe(map(data => _.values(data)))
+        ;
+
+
+      let parkHise: ParkirnaHisa[] = new Array();
+      this.http
+        .get('http://localhost:8080/Docker_rest/REST/parkirneHise')
+        .pipe(
+          map(data => _.values(data)),
+          tap(parkirneHise => this.dataSource = new MatTableDataSource(parkirneHise))    // users array [Object, Object, Object]
+        )
+        .subscribe(parkirneHise => parkHise = parkirneHise);
+
     }
 
     ngOnInit() {
         this.dataSource.paginator = this.paginator;
         this.dataSource.sort = this.sort;
-        this.parkirneHise$ = this.http
-        .get<ParkirnaHisa[]>('http://localhost:8080/Docker_rest/REST/parkirneHise')
-          .pipe(map(data => _.values(data)))
-        ;
+
     }
 
     applyFilter(filterValue: string) {
@@ -102,23 +107,24 @@ const NAMES = [
 
 export interface UserData {
     id: string;
-    name: string;
+    naziv: string;
     zasedenost: string;
-    color: string;
+    lastnik: string;
 }
 
 /** Builds and returns a new User. */
 function createNewUser(id: number): UserData {
-    const name =
+    const naziv =
         NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
         ' ' +
         NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) +
         '.';
+    const lastnik = 'David';
 
     return {
         id: id.toString(),
-        name: name,
+        naziv: naziv,
         zasedenost: Math.round(Math.random() * 100).toString(),
-        color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
+        lastnik: lastnik
     };
 }
