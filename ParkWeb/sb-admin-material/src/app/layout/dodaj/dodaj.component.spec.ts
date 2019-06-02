375d835fa4eb1d2f85bd4756b4517dbf41ede23f
David Žele <david.zele@student.um.si>
2019-06-02 16:06:58 +0200

    Push posodobljene graficne podobe in popravki

--- ParkWeb/sb-admin-material/src/app/layout/dodaj/dodaj.component.spec.ts
+++ ParkWeb/sb-admin-material/src/app/layout/dodaj/dodaj.component.spec.ts
@@ -1,19 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DialogOverviewComponent } from './dialog-overview.component';
+import { DodajComponent } from './dodaj.component';
 
 describe('DodajComponent', () => {
-    let component: DialogOverviewComponent;
-    let fixture: ComponentFixture<DialogOverviewComponent>;
+    let component: DodajComponent;
+    let fixture: ComponentFixture<DodajComponent>;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [DialogOverviewComponent]
+            declarations: [DodajComponent]
         }).compileComponents();
     }));
 
     beforeEach(() => {
-        fixture = TestBed.createComponent(DialogOverviewComponent);
+        fixture = TestBed.createComponent(DodajComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
--- ParkWeb/sb-admin-material/src/app/layout/tables/tables.module.ts
+++ ParkWeb/sb-admin-material/src/app/layout/tables/tables.module.ts
@@ -1,21 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatTableModule } from '@angular/material';
+import {MatButtonModule, MatSortModule, MatTableModule} from '@angular/material';
 import { MatFormFieldModule, MatPaginatorModule } from '@angular/material';
 import { MatInputModule } from '@angular/material';
+import { MatDialogModule } from '@angular/material';
 
 import { TablesRoutingModule } from './tables-routing.module';
 import { TablesComponent } from './tables.component';
+import {DodajComponent} from '../dodaj/dodaj.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        TablesRoutingModule,
-        MatTableModule,
-        MatFormFieldModule,
-        MatPaginatorModule,
-        MatInputModule
-    ],
-    declarations: [TablesComponent]
-})
+  imports: [
+    CommonModule,
+    TablesRoutingModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSortModule,
+    MatDialogModule,
+    FormsModule
+  ],
+    declarations: [TablesComponent,
+      DodajComponent],
+  entryComponents: [DodajComponent],
+  })
 export class TablesModule {}
