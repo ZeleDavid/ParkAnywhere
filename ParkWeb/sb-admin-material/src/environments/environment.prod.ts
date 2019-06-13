45aeda8e1d1cf96bc509cf18db0838d166da9662
KovacZan <39158639+KovacZan@users.noreply.github.com>
2019-06-13 15:41:42 +0200

    production

--- ParkWeb/sb-admin-material/src/environments/environment.prod.ts
+++ ParkWeb/sb-admin-material/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA8RvHaOPP-HN7t01-FaukrQEPEO5zjTEo',
+    authDomain: 'praktikum2-22222.firebaseapp.com',
+    databaseURL: 'https://praktikum2-22222.firebaseio.com',
+    projectId: 'praktikum2-22222',
+    storageBucket: 'praktikum2-22222.appspot.com',
+    messagingSenderId: '563319093309',
+    appId: '1:563319093309:web:f17dc857a3d1fac8'
+  }
 };
